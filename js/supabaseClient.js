1e439513028e04d2caaddc834b14a9942fb51bc9
Ullas <118278305+Ullasgs@users.noreply.github.com>
2025-05-19 23:20:05 +0530

    Delete js directory

--- js/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = 'https://xyurvsejciodlcixqyqk.supabase.co'; // Replace with your Supabase project URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5dXJ2c2VqY2lvZGxjaXhxeXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NTgwNDUsImV4cCI6MjA2MzEzNDA0NX0.7qFLx-MqPHJZna2tko4LKN0xkTsxUMb6ye56bSpKGnI';
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
